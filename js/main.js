f95f8990008caa1db163031705b924c1830c2ec8
Yuri Alves <yurialvesguernsey@outlook.com>
2018-12-10 01:30:52 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
-var Tips = [{
-  "Tip": "Tip #1",
-   "User": "Yuri Alves"
-},
-{
-  "Tip": "Tip #2",
-   "User": "Yuri Alves"
+$(document).ready(function () {
+  // VueJS data
+  var App = new Vue({
+    el: "#Tips"
+    , data: {
+      json: []
+    , }
+    , created: function () {
+      ajaxGetEvents().done(function (json) {
+        App.json = json;
+      }).fail(function (json) {
+        console.log(json);
+      });
+    }
+  });
+});
+var loc = "https://Startips.io"
+  // JSON API that gets all the events
+function ajaxGetEvents() {
+  return $.ajax({
+    type: 'GET'
+    , url: loc + "/js/tips.json"
+    , contentType: 'application/json'
+    , processData: false
+  }).done(function () {
+    App.loading = false;
+  });
 }
-];
-
-console.log(Tips);
